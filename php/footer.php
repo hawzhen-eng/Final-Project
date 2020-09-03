2934af80af78d501bd5756a4316a0ce8a8d4a9d1
hawzhen-eng <h2zhena7@gmail.com>
2020-09-03 01:43:42 -0400

    organized

---
+++ php/footer.php
@@ -0,0 +1,59 @@
+<?php
+echo '  
+
+    <footer>
+        <div class="main-content ">
+
+            <div class="left box ">
+
+                <a href="aboutus.php">
+                    <h2>About Us</h2>
+                </a>
+
+                <div class="content ">
+                    <p>
+                        <em>* The site is a repository of adoptable dogs rescued by individuals or groups in WNY *.</em>
+                    </p>
+                    <div class="social ">
+                        <a href="# "><span class="fab fa-facebook-f "></span></a>
+                        <a href="# "><span class="fab fa-twitter "></span></a>
+                        <a href="# "><span class="fab fa-instagram "></span></a>
+                        <a href="# "><span class="fab fa-youtube "></span></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="center box ">
+                <h2>contact Us</h2>
+                <div class="content ">
+                    <div class="place ">
+                        <span class="fas fa-map-marker-alt "></span>
+                        <span class="text ">1234 Main St.Buffalo</span>
+                    </div>
+                    <div class="phone ">
+                        <span class="fas fa-phone-alt "></span>
+                        <span class="text ">(716)011-2345</span>
+                    </div>
+                    <div class="email ">
+                        <span class="fas fa-envelope "></span>
+                        <span class="text ">RescueMe!@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="right box">
+
+                <a href="adminpage.php">
+                    <h2>Admin</h2>
+                </a>
+
+                <div class="content ">
+                </div>
+                <dr/>
+            </div>
+        </div>
+
+        </div>
+
+    </footer>
+ ';
